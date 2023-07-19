1c3c0e373a8f318962b62a86afc2d35d04e8baaf
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-07-19 15:56:27 +0700

    Create Header

---
+++ app/header/page.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="flex justify-center mt-8 text-center">
+      <div>
+        <h1 className="text-5xl font-bold">Gym Tracker 🏋️‍♂️</h1>
+        <br />
+        <p className="text-lg italic">{`{Insert Verse of the Day}`}</p>
+      </div>
+    </div>
+  )
+}
